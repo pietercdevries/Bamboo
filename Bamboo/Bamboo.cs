27d174f8e35f1a0414e2aa53215c3ccd48a37c9a
pieterdevries <pieterandcamryn@gmail.com>
2017-02-07 20:24:51 -0800

    Added Transactions Now Save Changes must be called.

--- Bamboo/Bamboo.cs
+++ Bamboo/Bamboo.cs
@@ -13,11 +13,14 @@ namespace Bamboo
 
         private string connectionString { get; set; }
 
+        private List<KeyValuePair<string, object>> transactions = new List<KeyValuePair<string, object>>();
+
         //Sync Methods
-        public void Add(object item, string objectName, string[] paramaterNames)
+        public void Add(object item, string[] paramaterNames)
         {
             string propertyNames = "";
             string propertyParamaters = "";
+            string itemName = item.GetType().Name;
 
             int propertyCount = paramaterNames.Count();
             for (int I = 0; I < propertyCount; I++)
@@ -34,29 +37,15 @@ namespace Bamboo
                 }
             }
 
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                using (var transaction = sqlConnection.BeginTransaction())
-                {
-                    try
-                    {
-                        sqlConnection.Execute($"Insert Into [{objectName}] ({propertyNames}) Values ({propertyParamaters})", item, transaction);
-                        transaction.Commit();
-                    }
-                    catch
-                    {
-                        transaction.Rollback();
-                    }
-                }
-                sqlConnection.Close();
-            }
+            KeyValuePair<string, object> command = new KeyValuePair<string, object>($"Insert Into[{ itemName}] ({ propertyNames}) Values({ propertyParamaters})", item);
+            transactions.Add(command);
         }
 
-        public void Add(List<object> items, string objectName, string[] paramaterNames)
+        public void Add(List<object> items, string[] paramaterNames)
         {
             string propertyNames = "";
             string propertyParamaters = "";
+            string itemName = items.First().GetType().Name;
 
             int propertyCount = paramaterNames.Count();
             for (int I = 0; I < propertyCount; I++)
@@ -73,28 +62,14 @@ namespace Bamboo
                 }
             }
 
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                using (var transaction = sqlConnection.BeginTransaction())
-                {
-                    try
-                    {
-                        sqlConnection.Execute($"Insert Into [{objectName}] ({propertyNames}) Values ({propertyParamaters})", items, transaction);
-                        transaction.Commit();
-                    }
-                    catch
-                    {
-                        transaction.Rollback();
-                    }
-                }
-                sqlConnection.Close();
-            }
+            KeyValuePair<string, object> command = new KeyValuePair<string, object>($"Insert Into [{itemName}] ({propertyNames}) Values ({propertyParamaters})", items);
+            transactions.Add(command);
         }
 
-        public void Edit(object item, string objectName, string[] paramaterNames)
+        public void Edit(object item, string[] paramaterNames)
         {
             string properties = "";
+            string itemName = item.GetType().Name;
 
             int propertyCount = paramaterNames.Count();
             for (int I = 0; I < propertyCount; I++)
@@ -112,28 +87,14 @@ namespace Bamboo
                 }
             }
 
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                using (var transaction = sqlConnection.BeginTransaction())
-                {
-                    try
-                    {
-                        sqlConnection.Execute($"Update [{objectName}] SET {properties} Where Id = @Id", item, transaction);
-                        transaction.Commit();
-                    }
-                    catch
-                    {
-                        transaction.Rollback();
-                    }
-                }
-                sqlConnection.Close();
-            }
+            KeyValuePair<string, object> command = new KeyValuePair<string, object>($"Update [{itemName}] SET {properties} Where Id = @Id", item);
+            transactions.Add(command);
         }
 
-        public void Edit(List<object> items, string objectName, string[] paramaterNames)
+        public void Edit(List<object> items, string[] paramaterNames)
         {
             string properties = "";
+            string itemName = items.First().GetType().Name;
 
             int propertyCount = paramaterNames.Count();
             for (int I = 0; I < propertyCount; I++)
@@ -151,64 +112,27 @@ namespace Bamboo
                 }
             }
 
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                using (var transaction = sqlConnection.BeginTransaction())
-                {
-                    try
-                    {
-                        sqlConnection.Execute($"Update [{objectName}] SET {properties} Where Id = @Id", items, transaction);
-                        transaction.Commit();
-                    }
-                    catch
-                    {
-                        transaction.Rollback();
-                    }
-                }
-                sqlConnection.Close();
-            }
+            KeyValuePair<string, object> command = new KeyValuePair<string, object>($"Update [{itemName}] SET {properties} Where Id = @Id", items);
+            transactions.Add(command);
         }
 
         public void Delete(object item, string objectName)
         {
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            if (item != null)
             {
-                sqlConnection.Open();
-                using (var transaction = sqlConnection.BeginTransaction())
-                {
-                    try
-                    {
-                        sqlConnection.Execute($"DELETE FROM [{objectName}] Where Id = @Id", item, transaction);
-                        transaction.Commit();
-                    }
-                    catch
-                    {
-                        transaction.Rollback();
-                    }
-                }
-                sqlConnection.Close();
+                string itemName = item.GetType().Name;
+                KeyValuePair<string, object> command = new KeyValuePair<string, object>($"DELETE FROM [{itemName}] Where Id = @Id", item);
+                transactions.Add(command);
             }
         }
 
-        public void Delete(List<object> items, string objectName)
+        public void Delete(List<object> items)
         {
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            if (items != null)
             {
-                sqlConnection.Open();
-                using (var transaction = sqlConnection.BeginTransaction())
-                {
-                    try
-                    {
-                        sqlConnection.Execute($"DELETE FROM [{objectName}] Where Id = @Id", items, transaction);
-                        transaction.Commit();
-                    }
-                    catch
-                    {
-                        transaction.Rollback();
-                    }
-                }
-                sqlConnection.Close();
+                string itemName = items.First().GetType().Name;
+                KeyValuePair<string, object> command = new KeyValuePair<string, object>($"DELETE FROM [{itemName}] Where Id = @Id", items);
+                transactions.Add(command);
             }
         }
 
@@ -344,7 +268,32 @@ namespace Bamboo
                 sqlConnection.Close();
             }
         }
- 
+
+        public void SaveChanges()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (var transaction = sqlConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        foreach (KeyValuePair<string, object> command in transactions)
+                        {
+                            sqlConnection.Execute(command.Key, command.Value, transaction);
+                        }
+
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+                sqlConnection.Close();
+            }
+        }
 
         public void newConnection(string connection)
         {
