f6e3793d9f576e317b373f59b128132ea204b7ac
pieterdevries <pieterandcamryn@gmail.com>
2017-02-08 00:27:07 -0800

    Added a way to not use transactions

--- Bamboo/Bamboo.cs
+++ Bamboo/Bamboo.cs
@@ -15,7 +15,6 @@ namespace Bamboo
 
         private List<KeyValuePair<string, object>> transactions = new List<KeyValuePair<string, object>>();
 
-        //Sync Methods
         public void Add(object item, string[] paramaterNames)
         {
             string propertyNames = "";
@@ -116,7 +115,7 @@ namespace Bamboo
             transactions.Add(command);
         }
 
-        public void Delete(object item, string objectName)
+        public void Delete(object item)
         {
             string itemName = item.GetType().Name;
             KeyValuePair<string, object> command = new KeyValuePair<string, object>($"DELETE FROM [{itemName}] Where Id = @Id", item);
@@ -130,168 +129,295 @@ namespace Bamboo
             transactions.Add(command);
         }
 
-        //Async Methods
-        public void AddAsync(object item, string[] paramaterNames)
-        {
-            string propertyNames = "";
-            string propertyParamaters = "";
-            string itemName = item.GetType().Name;
-
-            int propertyCount = paramaterNames.Count();
-            for (int I = 0; I < propertyCount; I++)
-            {
-                if (I == propertyCount - 1)
-                {
-                    propertyNames += "[" + paramaterNames[I] + "]";
-                    propertyParamaters += "@" + paramaterNames[I];
-                }
-                else
-                {
-                    propertyNames += "[" + paramaterNames[I] + "],";
-                    propertyParamaters += "@" + paramaterNames[I] + ",";
-                }
-            }
-
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                sqlConnection.ExecuteAsync($"Insert Into [{itemName}] ({propertyNames}) Values ({propertyParamaters})", item);
-                sqlConnection.Close();
-            }
-        }
-
-        public void AddAsync(List<object> items, string[] paramaterNames)
-        {
-            string propertyNames = "";
-            string propertyParamaters = "";
-            string itemName = items.First().GetType().Name;
-
-            int propertyCount = paramaterNames.Count();
-            for (int I = 0; I < propertyCount; I++)
-            {
-                if (I == propertyCount - 1)
-                {
-                    propertyNames += "[" + paramaterNames[I] + "]";
-                    propertyParamaters += "@" + paramaterNames[I];
-                }
-                else
-                {
-                    propertyNames += "[" + paramaterNames[I] + "],";
-                    propertyParamaters += "@" + paramaterNames[I] + ",";
-                }
-            }
-
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                sqlConnection.ExecuteAsync($"Insert Into [{itemName}] ({propertyNames}) Values ({propertyParamaters})", items);
-                sqlConnection.Close();
-            }
-        }
-
-        public void EditAsync(object item, string[] paramaterNames)
-        {
-            string properties = "";
-            string itemName = item.GetType().Name;
-
-            int propertyCount = paramaterNames.Count();
-            for (int I = 0; I < propertyCount; I++)
-            {
-                if (paramaterNames[I] != null)
-                {
-                    if (I == propertyCount - 1)
-                    {
-                        properties += "[" + paramaterNames[I] + "] = " + "@" + paramaterNames[I]; ;
-                    }
-                    else
-                    {
-                        properties += "[" + paramaterNames[I] + "] = " + "@" + paramaterNames[I] + ", ";
-                    }
-                }
-            }
-
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                sqlConnection.ExecuteAsync($"Update [{itemName}] SET {properties} Where Id = @Id", item);
-                sqlConnection.Close();
-            }
-        }
-
-        public void EditAsync(List<object> items, string[] paramaterNames)
-        {
-            string properties = "";
-            string itemName = items.First().GetType().Name;
-
-            int propertyCount = paramaterNames.Count();
-            for (int I = 0; I < propertyCount; I++)
-            {
-                if (paramaterNames[I] != null)
-                {
-                    if (I == propertyCount - 1)
-                    {
-                        properties += "[" + paramaterNames[I] + "] = " + "@" + paramaterNames[I]; ;
-                    }
-                    else
-                    {
-                        properties += "[" + paramaterNames[I] + "] = " + "@" + paramaterNames[I] + ", ";
-                    }
-                }
-            }
-
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                sqlConnection.ExecuteAsync($"Update [{itemName}] SET {properties} Where Id = @Id", items);
-                sqlConnection.Close();
-            }
-        }
-
-        public void DeleteAsync(object item)
-        {
-            string itemName = item.GetType().Name;
-
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                sqlConnection.ExecuteAsync($"DELETE FROM [{itemName}] Where Id = @Id", item);
-                sqlConnection.Close();
-            }
-        }
-
-        public void DeleteAsync(List<object> items)
-        {
-            string itemName = items.First().GetType().Name;
-
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-                sqlConnection.ExecuteAsync($"DELETE FROM [{itemName}] Where Id = @Id", items);
-                sqlConnection.Close();
-            }
-        }
-
-
         public void SaveChanges()
         {
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
                 sqlConnection.Open();
-                using (var transaction = sqlConnection.BeginTransaction())
+                using (SqlTransaction sqlTransaction = sqlConnection.BeginTransaction())
                 {
                     try
                     {
                         foreach (KeyValuePair<string, object> command in transactions)
                         {
-                            sqlConnection.Execute(command.Key, command.Value, transaction);
+                            sqlConnection.Execute(command.Key, command.Value, sqlTransaction);
                         }
 
-                        transaction.Commit();
+                        sqlTransaction.Commit();
                     }
                     catch
                     {
-                        transaction.Rollback();
+                        sqlTransaction.Rollback();
                         throw;
+                    }
+                    finally
+                    {
+                        sqlConnection.Close();
+                        transactions.Clear();
+                    }
+                }
+                sqlConnection.Close();
+            }
+
+            transactions.Clear();
+        }
+
+        public void SaveChangesAsync()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    foreach (KeyValuePair<string, object> command in transactions)
+                    {
+                        sqlConnection.ExecuteAsync(command.Key, command.Value);
+                    }
+                    sqlConnection.Close();
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                    transactions.Clear();
+                }
+            }
+
+            transactions.Clear();
+        }
+
+        public void CancelChanges()
+        {
+            transactions.Clear();
+        }
+
+        public void ForceAdd(object item, string[] paramaterNames)
+        {
+            string propertyNames = "";
+            string propertyParamaters = "";
+            string itemName = item.GetType().Name;
+
+            int propertyCount = paramaterNames.Count();
+            for (int I = 0; I < propertyCount; I++)
+            {
+                if (I == propertyCount - 1)
+                {
+                    propertyNames += "[" + paramaterNames[I] + "]";
+                    propertyParamaters += "@" + paramaterNames[I];
+                }
+                else
+                {
+                    propertyNames += "[" + paramaterNames[I] + "],";
+                    propertyParamaters += "@" + paramaterNames[I] + ",";
+                }
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlTransaction sqlTransaction = sqlConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        sqlConnection.Execute($"Insert Into [{itemName}] ({propertyNames}) Values ({propertyParamaters})", item, sqlTransaction);
+                        sqlTransaction.Commit();
+                    }
+                    catch
+                    {
+                        sqlTransaction.Rollback();
+                    }
+                    finally
+                    {
+                        sqlConnection.Close();
+                    }
+                }
+                sqlConnection.Close();
+            }
+        }
+
+        public void ForceAdd(List<object> items, string[] paramaterNames)
+        {
+            string propertyNames = "";
+            string propertyParamaters = "";
+            string itemName = items.First().GetType().Name;
+
+            int propertyCount = paramaterNames.Count();
+            for (int I = 0; I < propertyCount; I++)
+            {
+                if (I == propertyCount - 1)
+                {
+                    propertyNames += "[" + paramaterNames[I] + "]";
+                    propertyParamaters += "@" + paramaterNames[I];
+                }
+                else
+                {
+                    propertyNames += "[" + paramaterNames[I] + "],";
+                    propertyParamaters += "@" + paramaterNames[I] + ",";
+                }
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlTransaction sqlTransaction = sqlConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        sqlConnection.Execute($"Insert Into [{itemName}] ({propertyNames}) Values ({propertyParamaters})", items, sqlTransaction);
+                        sqlTransaction.Commit();
+                    }
+                    catch
+                    {
+                        sqlTransaction.Rollback();
+                    }
+                    finally
+                    {
+                        sqlConnection.Close();
+                    }
+                }
+                sqlConnection.Close();
+            }
+        }
+
+        public void ForceEdit(object item, string[] paramaterNames)
+        {
+            string properties = "";
+            string itemName = item.GetType().Name;
+
+            int propertyCount = paramaterNames.Count();
+            for (int I = 0; I < propertyCount; I++)
+            {
+                if (paramaterNames[I] != null)
+                {
+                    if (I == propertyCount - 1)
+                    {
+                        properties += "[" + paramaterNames[I] + "] = " + "@" + paramaterNames[I]; ;
+                    }
+                    else
+                    {
+                        properties += "[" + paramaterNames[I] + "] = " + "@" + paramaterNames[I] + ", ";
+                    }
+                }
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlTransaction sqlTransaction = sqlConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        sqlConnection.Execute($"Update [{itemName}] SET {properties} Where Id = @Id", item, sqlTransaction);
+                        sqlTransaction.Commit();
+                    }
+                    catch
+                    {
+                        sqlTransaction.Rollback();
+                    }
+                    finally
+                    {
+                        sqlConnection.Close();
+                    }
+                }
+                sqlConnection.Close();
+            }
+        }
+
+        public void ForceEdit(List<object> items, string[] paramaterNames)
+        {
+            string properties = "";
+            string itemName = items.First().GetType().Name;
+
+            int propertyCount = paramaterNames.Count();
+            for (int I = 0; I < propertyCount; I++)
+            {
+                if (paramaterNames[I] != null)
+                {
+                    if (I == propertyCount - 1)
+                    {
+                        properties += "[" + paramaterNames[I] + "] = " + "@" + paramaterNames[I]; ;
+                    }
+                    else
+                    {
+                        properties += "[" + paramaterNames[I] + "] = " + "@" + paramaterNames[I] + ", ";
+                    }
+                }
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlTransaction sqlTransaction = sqlConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        sqlConnection.Execute($"Update [{itemName}] SET {properties} Where Id = @Id", items, sqlTransaction);
+                        sqlTransaction.Commit();
+                    }
+                    catch
+                    {
+                        sqlTransaction.Rollback();
+                    }
+                    finally
+                    {
+                        sqlConnection.Close();
+                    }
+                }
+                sqlConnection.Close();
+            }
+        }
+
+        public void ForceDelete(object item)
+        {
+            string itemName = item.GetType().Name;
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlTransaction sqlTransaction = sqlConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        sqlConnection.Execute($"DELETE FROM [{itemName}] Where Id = @Id", item, sqlTransaction);
+                        sqlTransaction.Commit();
+                    }
+                    catch
+                    {
+                        sqlTransaction.Rollback();
+                    }
+                    finally
+                    {
+                        sqlConnection.Close();
+                    }
+                }
+                sqlConnection.Close();
+            }
+        }
+
+        public void ForceDelete(List<object> items)
+        {
+            string itemName = items.First().GetType().Name;
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                using (SqlTransaction sqlTransaction = sqlConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        sqlConnection.Execute($"DELETE FROM [{itemName}] Where Id = @Id", items, sqlTransaction);
+                        sqlTransaction.Commit();
+                    }
+                    catch
+                    {
+                        sqlTransaction.Rollback();
+                    }
+                    finally
+                    {
+                        sqlConnection.Close();
                     }
                 }
                 sqlConnection.Close();
